b875b865d7458715ae764d7e642701bdf9d2838b
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-07-01 12:25:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
 //your JS code here. If required.
-const student={
-	name:"john",
-}
 
-function getKeys(obj){
-	return Object.keys(obj);
-}
 
-console.log(getKeys(student));
-
-// const student2={
-// 	name:"abc",
-// 	city:"edf",
-// }
-// console.log(getkeys(student2));
